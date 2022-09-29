a10b66252043f9b722affea94e2d9607e1b1ba83
Communist17 <114125595+Communist17@users.noreply.github.com>
2022-09-29 20:52:50 +0800

    Add files via upload

---
+++ 99.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+int main ()
+{
+    int i,j;
+    for (i=1;i<=9;i++)
+    {
+        for (j=1; j<=i;j++)
+        printf("%d*%d=%d\t",i,j,i*j);
+        printf("\n");
+
+    }
+    system("pause");
+    return 0;
+}
